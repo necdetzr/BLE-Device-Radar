0324c8f6f52031ba8196999f68b53593c71ff5eb
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-07-13 21:35:12 +0300

    Serializable added into nav key.

--- feature/history/src/main/java/com/necdetzr/history/navigation/HistoryEntryProvider.kt
+++ feature/history/src/main/java/com/necdetzr/history/navigation/HistoryEntryProvider.kt
@@ -4,9 +4,10 @@ import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.necdetzr.history.HistoryScreen
 import com.necdetzr.history.api.HistoryNavKey
+import com.necdetzr.navigation.Navigator
 
 
-fun EntryProviderScope<NavKey>.historyEntry(){
+fun EntryProviderScope<NavKey>.historyEntry(navigator: Navigator){
     entry<HistoryNavKey>{
         HistoryScreen()
     }
