d96b57d809395422d4a72ad433233a69aacb6f46
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-07-13 21:37:02 +0300

    User preferences data added.

--- core/data/src/main/java/com/necdetzr/data/repository/OfflineUserDataRepository.kt
+++ core/data/src/main/java/com/necdetzr/data/repository/OfflineUserDataRepository.kt
@@ -3,11 +3,15 @@ package com.necdetzr.data.repository
 import com.necdetzr.datastore.BlePreferencesDataSource
 import com.necdetzr.model.SortType
 import com.necdetzr.model.ThemeConfig
+import com.necdetzr.model.UserPreferences
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class OfflineUserDataRepository @Inject constructor(
     private val blePreferencesDataSource: BlePreferencesDataSource
 ) : UserDataRepository{
+    override val userPreferences: Flow<UserPreferences> =
+        blePreferencesDataSource.userData
     override suspend fun setScanPeriod(scanPeriod: Long) {
         blePreferencesDataSource.updateScanPeriod(scanPeriod)
     }
