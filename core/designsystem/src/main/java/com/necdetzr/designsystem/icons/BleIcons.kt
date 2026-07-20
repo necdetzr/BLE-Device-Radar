f82ab7ac359a08fd7b1d7f8539a5bcd15fbd89c5
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-07-21 00:20:03 +0300

    New icons added.

--- core/designsystem/src/main/java/com/necdetzr/designsystem/icons/BleIcons.kt
+++ core/designsystem/src/main/java/com/necdetzr/designsystem/icons/BleIcons.kt
@@ -2,9 +2,12 @@ package com.necdetzr.designsystem.icons
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.BluetoothSearching
+import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.PlayCircleFilled
 import androidx.compose.material.icons.filled.Radar
+import androidx.compose.material.icons.filled.SettingsSuggest
 import androidx.compose.material.icons.filled.SignalCellularAlt
 import androidx.compose.material.icons.filled.SignalCellularAlt1Bar
 import androidx.compose.material.icons.filled.SignalCellularAlt2Bar
@@ -29,4 +32,7 @@ object BleIcons {
     val RssiHigh = Icons.Default.SignalCellularAlt
     val Start = Icons.Default.PlayCircleFilled
     val Stop = Icons.Default.StopCircle
+    val Dark = Icons.Default.DarkMode
+    val Light = Icons.Default.LightMode
+    val System = Icons.Default.SettingsSuggest
 }
