61faaa61dab962bdbf16359e985a7fc125db287f
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-07-27 20:21:29 +0300

    Model Changed.

--- core/data/src/main/java/com/necdetzr/data/repository/BleRadarRepository.kt
+++ core/data/src/main/java/com/necdetzr/data/repository/BleRadarRepository.kt
@@ -1,9 +1,9 @@
 package com.necdetzr.data.repository
 
 import com.necdetzr.common.result.Result
-import com.necdetzr.model.BleDevice
+import com.necdetzr.model.ScannedBleDevice
 import kotlinx.coroutines.flow.Flow
 
 interface BleRadarRepository {
-    fun startScanning(): Flow<Result<BleDevice>>
+    fun startScanning(): Flow<Result<ScannedBleDevice>>
 }
