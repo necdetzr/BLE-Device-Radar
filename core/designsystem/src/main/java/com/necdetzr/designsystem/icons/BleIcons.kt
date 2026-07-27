11d84dc10d17f010a999f6af597d647f89a7dbf2
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-07-27 20:17:02 +0300

    New Icons added.

--- core/designsystem/src/main/java/com/necdetzr/designsystem/icons/BleIcons.kt
+++ core/designsystem/src/main/java/com/necdetzr/designsystem/icons/BleIcons.kt
@@ -2,11 +2,25 @@ package com.necdetzr.designsystem.icons
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.BluetoothSearching
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Factory
+import androidx.compose.material.icons.filled.Fingerprint
 import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Inbox
 import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material.icons.filled.Link
+import androidx.compose.material.icons.filled.LocationSearching
+import androidx.compose.material.icons.filled.MiscellaneousServices
 import androidx.compose.material.icons.filled.PlayCircleFilled
+import androidx.compose.material.icons.filled.PowerInput
 import androidx.compose.material.icons.filled.Radar
+import androidx.compose.material.icons.filled.Sensors
 import androidx.compose.material.icons.filled.SettingsSuggest
 import androidx.compose.material.icons.filled.SignalCellularAlt
 import androidx.compose.material.icons.filled.SignalCellularAlt1Bar
@@ -35,4 +49,18 @@ object BleIcons {
     val Dark = Icons.Default.DarkMode
     val Light = Icons.Default.LightMode
     val System = Icons.Default.SettingsSuggest
+    val Fingerprint = Icons.Default.Fingerprint
+    val Copy = Icons.Default.ContentCopy
+    val Done = Icons.Default.Done
+    val DownArrow = Icons.Default.ArrowDropDown
+    val UpArrow = Icons.Default.ArrowDropUp
+    val Distance = Icons.Default.LocationSearching
+    val Time = Icons.Default.AccessTime
+    val Energy = Icons.Default.PowerInput
+    val Connect = Icons.Default.Link
+    val Packet = Icons.Default.Inbox
+    val Code = Icons.Default.Code
+    val Services = Icons.Default.MiscellaneousServices
+    val Manufacturer = Icons.Default.Factory
+    val Sensors = Icons.Default.Sensors
 }
