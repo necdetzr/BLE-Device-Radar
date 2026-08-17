6bd6da91ed2c9cb7030faa0828617f632f03fd17
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-08-17 23:11:07 +0300

    History navigation completed.

--- feature/history/src/main/java/com/necdetzr/history/navigation/HistoryEntryProvider.kt
+++ feature/history/src/main/java/com/necdetzr/history/navigation/HistoryEntryProvider.kt
@@ -4,13 +4,16 @@ import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.necdetzr.history.HistoryScreen
 import com.necdetzr.history.api.HistoryNavKey
+import com.necdetzr.history.api.HistorySearchNavKey
 import com.necdetzr.navigation.Navigator
 
 
 fun EntryProviderScope<NavKey>.historyEntry(navigator: Navigator){
     entry<HistoryNavKey>{
         HistoryScreen(
-            onSearchClick = {}
+            onSearchClick = {
+                navigator.navigate(HistorySearchNavKey)
+            }
         )
     }
 }
--- feature/history/src/main/java/com/necdetzr/history/navigation/HistorySearchEntryProvider.kt
+++ feature/history/src/main/java/com/necdetzr/history/navigation/HistorySearchEntryProvider.kt
@@ -1,2 +1,15 @@
 package com.necdetzr.history.navigation
 
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+import com.necdetzr.history.api.HistorySearchNavKey
+import com.necdetzr.history.search.HistorySearchScreen
+import com.necdetzr.navigation.Navigator
+
+fun EntryProviderScope<NavKey>.historySearch(navigator: Navigator){
+    entry<HistorySearchNavKey> {
+        HistorySearchScreen(
+            onBackButton = { navigator.goBack() }
+        )
+    }
+}
