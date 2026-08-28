50c90c634ea745bcdfb488692a093ebd712f501d
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-08-28 23:50:08 +0300

    chore: Unused imports deleted.

--- feature/history/src/main/java/com/necdetzr/history/components/DeviceSearchItem.kt
+++ feature/history/src/main/java/com/necdetzr/history/components/DeviceSearchItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
