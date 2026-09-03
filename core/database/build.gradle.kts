08d23408d7fba775891141dee4615fba84d242d8
Necdet <117301669+AhplaXx@users.noreply.github.com>
2026-09-03 23:36:37 +0300

    chore: test dependencies added.

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -14,4 +14,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(project(":core:model"))
     implementation(libs.kotlinx.serialization.json)
+    testImplementation(libs.junit)
+    testImplementation(libs.truth)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.truth)
+    androidTestImplementation(libs.room.testing)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.androidx.test.runner)
 }
